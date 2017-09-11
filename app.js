99c65d64ab70cd95b390becb2bbfcab6c80d8d1b
Safarali <mr.safarali@gmail.com>
2017-09-10 18:03:34 -0700

    Plan app

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+/* =============== Data Controller ================*/
 
+var dataController= (function () {
+
+    // Get initial data
+
+    //
+
+})();
+
+
+/* =============== UI Controller ===============*/
+
+var UIController = (function () {
+    // Display initial data
+
+})();
+
+
+
+/* ================== App Controller =========== */
+
+var controller = (function (dataCtrl, UICtrl) {
+    // Set Up Event Listeners
+
+    // Initilize App
+
+})(dataController, UIController);
